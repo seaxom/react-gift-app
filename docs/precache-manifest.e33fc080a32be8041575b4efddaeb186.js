4f41498be7a7e3504a2245cfff3bccd052fdec8d
Sergio Adrián Ortiz <seaxom@yahoo.com>
2020-09-14 19:08:13 -0500

    Docs agregado

---
+++ docs/precache-manifest.e33fc080a32be8041575b4efddaeb186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7433d35fc8c8fc9798c06fbfbb7facf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5df5f543471718529f6",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "71e0955d3f201ec0605f",
+    "url": "/static/css/main.0dc2e087.chunk.css"
+  },
+  {
+    "revision": "f5df5f543471718529f6",
+    "url": "/static/js/2.38a2c70c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38a2c70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e0955d3f201ec0605f",
+    "url": "/static/js/main.a1271de0.chunk.js"
+  },
+  {
+    "revision": "af54f6eaae89fdd75055",
+    "url": "/static/js/runtime-main.79c00166.js"
+  }
+]);
